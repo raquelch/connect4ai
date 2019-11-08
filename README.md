90c3898e413c6fbbb2f02185a2953b74b365cc0b
amellis4 <amellis4@BE-1035-005>
2019-11-08 10:44:11 -0600

    Fixed Error

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cs478group
-We are making a connect four game that will be played in a web page. This connect four game will allow the user to choose between playing player vs player, player vs ai, or ai vs ai. For the ai move selection we will use a min max algorithm to find the best possible move. Because we are using a min max algorithm we will be able to set the difficulty of the ai by selecting the depth of search used. Some other features we are hoping to add is player name input, color chip selection, and also a five in a row game.
+We are making a connect four game that will be played in a web page. This connect four game will allow the user to choose between
+playing player vs player, player vs ai, or ai vs ai. For the ai move selection we will use a min max algorithm to find the best
+possible move. Because we are using a min max algorithm, we will be able to set the difficulty of the ai by selecting the depth of
+search used. Some other features we are hoping to add is player name input, color chip selection, and also a five in a row game.
---
+++ fourInARow/fourInARow/fourInARow/GameForm.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace fourInARow
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.levelToolStripMenuItem = new System.Windows.Forms.ToolStripComboBox();
+            this.elementToolStripComboBox = new System.Windows.Forms.ToolStripComboBox();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripComboBox();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Goodluck";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Welcome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Have Fun";
+            // 
+            // levelToolStripMenuItem
+            // 
+            this.levelToolStripMenuItem.Items.AddRange(new object[] {
+            "Easy",
+            "Hard",
+            "Don\'t even try"});
+            this.levelToolStripMenuItem.Name = "levelToolStripMenuItem";
+            this.levelToolStripMenuItem.Size = new System.Drawing.Size(75, 23);
+            this.levelToolStripMenuItem.Text = "Level";
+            this.levelToolStripMenuItem.SelectedIndexChanged += new System.EventHandler(this.levelToolStripMenuItem_SelectedIndexChanged);
+            // 
+            // elementToolStripComboBox
+            // 
+            this.elementToolStripComboBox.Items.AddRange(new object[] {
+            "MiniMax",
+            "AplhaBeta"});
+            this.elementToolStripComboBox.Name = "elementToolStripComboBox";
+            this.elementToolStripComboBox.Size = new System.Drawing.Size(75, 23);
+            this.elementToolStripComboBox.Text = "Element";
+            // 
+            // modeToolStripMenuItem
+            // 
+            this.modeToolStripMenuItem.Items.AddRange(new object[] {
+            "P v P",
+            "P v AI"});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            this.modeToolStripMenuItem.Size = new System.Drawing.Size(75, 23);
+            this.modeToolStripMenuItem.Text = "Mode";
+            this.modeToolStripMenuItem.SelectedIndexChanged += new System.EventHandler(this.modeToolStripMenuItem_SelectedIndexChanged);
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(48, 23);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 23);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.levelToolStripMenuItem,
+            this.elementToolStripComboBox,
+            this.modeToolStripMenuItem,
+            this.undoToolStripMenuItem,
+            this.resetToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(400, 27);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GameForm";
+            this.Text = "Connect four in a row";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameForm_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.GameForm_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolStripComboBox levelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox elementToolStripComboBox;
+        private System.Windows.Forms.ToolStripComboBox modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
